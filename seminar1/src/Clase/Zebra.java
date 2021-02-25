bc4e0f68df639baedae1cc976f6fca94738ed9ff
Raluca <Raluca@DESKTOP-3AP0D1L>
2021-02-25 13:33:30 +0200

    zebra

---
+++ seminar1/src/Clase/Zebra.java
@@ -0,0 +1,22 @@
+package Clase;
+
+public class Zebra extends Animal {
+	private int  nrDungi;
+	public Zebra(String nume) {
+		super(nume);
+		nrDungi=50;
+		// TODO Auto-generated constructor stub
+	}
+	public Zebra(String nume, int nrDungi) {
+		super(nume);
+		this.nrDungi = nrDungi;
+	}
+	
+
+
+
+	
+	
+	
+
+}
--- seminar1/src/pachet/Main.java
+++ seminar1/src/pachet/Main.java
@@ -1,6 +1,7 @@
 package pachet;
 
 import Clase.Girafa;
+import Clase.Zebra;
 import Clase.Zoo;
 
 public class Main {
@@ -10,9 +11,13 @@ public class Main {
 		Zoo zoo=new Zoo();
 		Girafa g1=new Girafa("Maia",300);
 		Girafa g2= new Girafa("Lola");
+		Zebra z1= new Zebra ("Z1",20);
+		Zebra z2=new Zebra("Z2");
 		
 		zoo.AddAnimal(g1);
 		zoo.AddAnimal(g2);
+		zoo.AddAnimal(z1);
+		zoo.AddAnimal(z2);
 		zoo.feedAllAnimals();
 		
 
